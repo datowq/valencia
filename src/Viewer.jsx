7930fa93f730b86d88fe6bb838e4db2a22f81cbb
datowq <steven.andrew.lew@gmail.com>
2023-09-12 06:38:17 -0600

    add: added selection feature; note: to get the animations to be smooth I'm using frameloop 'always' > 'demand' has better performance but does not animate smoothly

--- src/Viewer.jsx
+++ src/Viewer.jsx
@@ -1,14 +1,26 @@
 import { useState, Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, BakeShadows, Stage, Preload } from '@react-three/drei'
+import { useSpring, animated } from '@react-spring/three'
+import { Bloom, DepthOfField, EffectComposer, Noise, Vignette } from '@react-three/postprocessing'
 import { SNES } from './components/Snes'
 import { Cartridge } from './components/Cartridge'
 import { CanvasLoader } from "./Loader";
 
 export const Viewer = () => {
+
+    const [activeCartridge, setActive] = useState()
+    const [selectedCartridge, setSelected] = useState(-1)
+
+    const animations = {
+        0: useSpring({ scale: [activeCartridge === 0 ? 0.5 : 0.4, activeCartridge === 0 ? 0.5 : 0.4, activeCartridge === 0 ? 0.5 : 0.4] }),
+        1: useSpring({ scale: [activeCartridge === 1 ? 0.5 : 0.4, activeCartridge === 1 ? 0.5 : 0.4, activeCartridge === 1 ? 0.5 : 0.4] }),
+        2: useSpring({ scale: [activeCartridge === 2 ? 0.5 : 0.4, activeCartridge === 2 ? 0.5 : 0.4, activeCartridge === 2 ? 0.5 : 0.4] })
+    }
+
     return (
         <Canvas
-            frameloop='demand'
+            frameloop='always'
             shadows
             camera={{position: [0, 5, 15], fov: 25}}
             gl={{ preserveDrawingBuffer: true }}
@@ -16,14 +28,23 @@ export const Viewer = () => {
             <Suspense fallback={<CanvasLoader/>}>
                 <Stage adjustCamera={0.75} environment='city' intensity={0.6}>
                     {/* <Cartridge position={[0,0.6,-0.3]} scale={0.4}/> */}
-                    <Cartridge position={[0,3,-0.3]} scale={0.4}/>
-                    <Cartridge position={[3.5,3,-0.3]} scale={0.4}/>
-                    <Cartridge position={[-3.5,3,-0.3]} scale={0.4}/>
+                    <animated.group>
+                        <Cartridge onPointerOver={() => setActive(0)} 
+                            position={[-3.5, 3, -0.3]} 
+                            scale={animations[0].scale} />
+                        <Cartridge onPointerOver={() => setActive(1)} 
+                            position={[0, 3, -0.3]} 
+                            scale={animations[1].scale} />
+                        <Cartridge onPointerOver={() => setActive(2)} 
+                            position={[3.5, 3, -0.3]} 
+                            scale={animations[2].scale} />
+                    </animated.group>
                     <SNES/>
                 </Stage>
                 <BakeShadows />
-                <OrbitControls/>
+                <OrbitControls enableRotate={false} enableZoom={false} enablePan={false} />
             </Suspense>
+
             <Preload all/>
         </Canvas>
     )
--- src/components/Cartridge.jsx
+++ src/components/Cartridge.jsx
@@ -1,17 +1,18 @@
 import React, { useRef } from 'react'
 import { useGLTF } from '@react-three/drei'
+import { animated } from '@react-spring/three'
 
 export const Cartridge = (props) => {
   const { nodes, materials } = useGLTF('/cartridge-transformed.glb')
   return (
-    <group {...props} dispose={null}>
+    <animated.group {...props} dispose={null}>
       <group scale={[1, 1, 0.325]}>
         <mesh geometry={nodes.Mesh.geometry} material={materials.body} />
         <mesh geometry={nodes.Mesh_1.geometry} material={materials.backsticker} />
         <mesh geometry={nodes.Mesh_2.geometry} material={materials.frontsticker} />
         <mesh geometry={nodes.Mesh_3.geometry} material={materials.screws} />
       </group>
-    </group>
+    </animated.group>
   )
 }
 
